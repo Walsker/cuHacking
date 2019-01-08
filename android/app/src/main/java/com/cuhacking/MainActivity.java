03d11a5aa9a82fa27cbecdfaf87c7ebca7a2f32f
Walsker <wal.gatlei@gmail.com>
2019-01-08 00:21:01 -0500

    imported react-navigation and react-native-gesture-handler

--- android/app/src/main/java/com/cuhacking/MainActivity.java
+++ android/app/src/main/java/com/cuhacking/MainActivity.java
@@ -1,11 +1,19 @@
 package com.cuhacking;
 
 import com.facebook.react.ReactActivity;
+
+// React native splash screen imports
 import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
 
+// React Native gesture handler imports
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
+    // From react native splash screen
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -13,6 +21,17 @@ public class MainActivity extends ReactActivity {
         super.onCreate(savedInstanceState);
     }
 
+    // From react native gesture handler
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
+    }
+    
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
