adc84b79fe73fff08ed58cd44d789096bb91a532
Walsker <wal.gatlei@gmail.com>
2019-01-18 19:16:49 -0500

    added a sign out button

--- src/postAuth/aboutScreen/aboutPage.js
+++ src/postAuth/aboutScreen/aboutPage.js
@@ -1,21 +1,52 @@
 // React Native imports
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Alert, StyleSheet, Text, View} from 'react-native';
+
+// Redux import
+import {connect} from 'react-redux';
+import {deleteCredentials} from 'cuHacking/src/preAuth/signInScreen/actions';
 
 // Custom imports
 import {colors, textStyle} from 'cuHacking/src/common/appStyles';
+import {Button} from 'cuHacking/src/common';
 
-export default class AboutPage extends Component
+class AboutPage extends Component
 {
+	signOut()
+	{
+		Alert.alert(
+			"Are you sure?",
+			"To sign back in, scan the invitation code you received via email.",
+			[
+				{text: 'No', onPress: () => {}},
+				{text: 'Yes', onPress: () => 
+				{
+					this.props.deleteCredentials();
+					this.props.navigation.navigate("Loading");
+				}}
+			]
+		);
+	}
+
 	render()
 	{
 		return (
 			<View style = {styles.default}>
 				<Text>About page and sponsors</Text>
+				<Button
+					label = "Log out"
+					color = 'red'
+					labelColor = 'white'
+					inverted = {false}
+					action = {this.signOut.bind(this)}
+				/>
 			</View>
 		);
 	}
 }
+
+export default connect(null, {deleteCredentials})(AboutPage);
+
 
 const styles = StyleSheet.create(
 {
